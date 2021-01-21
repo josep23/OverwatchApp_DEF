4626ad806ab94090c7b6a5f8a371481952e41f1c
josep <jcuestav22@elpuig.xeill.net>
2021-01-21 16:21:12 +0100

    Initial commit

---
+++ app/src/main/java/com/example/overwatch_def/Menu.java
@@ -0,0 +1,47 @@
+package com.example.overwatch_def;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.example.overwatch_def.databinding.FragmentMenuBinding;
+
+public class Menu extends Fragment {
+    NavController navController;
+    private FragmentMenuBinding binding;
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return (binding = FragmentMenuBinding.inflate(inflater,container,false)).getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        navController = Navigation.findNavController(view);
+
+        binding.button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                navController.navigate(R.id.action_menu_to_informacion);
+            }
+        });
+
+        binding.button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                navController.navigate(R.id.action_menu_to_login);
+            }
+        });
+    }
+}
